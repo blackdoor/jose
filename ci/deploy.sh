39debd910f1aa52b1b68fb5c03152827f6062ea7
Nathaniel Fischer <nfischer921@gmail.com>
2019-04-04 17:12:04 -0700

    Update .travis.yml

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-echo $SIGNING_KEY | base64 --decode > ci/signing.key
-gpg --fast-import ci/signing.key
+#sudo mv /usr/bin/gpg /usr/bin/gpg1
+#sudo update-alternatives --install /usr/bin/gpg gnupg /usr/bin/gpg2 100
 
-PUBLISH_VERSION=$TRAVIS_TAG mill mill.scalalib.PublishModule/publishAll --sonatypeCreds $SONATYPE_NAME:$SONATYPE_PW --release true --gpgPassphrase $GPG_PW --publishArtifacts __.publishArtifacts
+echo $SIGNING_KEY | base64 --decode > signing.key
+gpg --import signing.key
+#rm signing.key
+
+which gpg gpg1 gpg2
+
+#gpg --list-secret-keys
+#gpg1 --list-secret-keys
+gpg --list-secret-keys
+ls -la $HOME/.gnupg
+#cat $HOME/.gnupg/gpg.conf
+
+# echo "default-key $GPG_NAME" >> $HOME/.gnupg/gpg.conf
+
+PUBLISH_VERSION=$TRAVIS_TAG mill -i --disable-ticker mill.scalalib.PublishModule/publishAll \
+	$SONATYPE_NAME:$SONATYPE_PW \
+	$GPG_PW \
+	__.publishArtifacts \
+	--release \
+	true

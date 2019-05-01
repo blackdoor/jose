3f52b43b56235ea4e2258793255a1b0ad075c284
Kha Yuen Kam <khayuen.kam@moovel.com>
2019-05-01 16:19:06 +0200

    Remove space when serializing

--- jose/json/circe/src/black/door/jose/json/circe/package.scala
+++ jose/json/circe/src/black/door/jose/json/circe/package.scala
@@ -9,7 +9,7 @@ import io.circe.syntax._
 
 package object circe {
   def jsonSerializer[A](implicit encoder: Encoder[A]): Mapper[A, Array[Byte]] =
-    a => Right(a.asJson.dropNullValues.toString().getBytes(StandardCharsets.UTF_8))
+    a => Right(a.asJson.dropNullValues.noSpaces.getBytes(StandardCharsets.UTF_8))
   def jsonDeserializer[A](implicit decoder: Decoder[A]): Mapper[Array[Byte], A] =
     bytes => decode(new String(bytes, StandardCharsets.UTF_8)).left.map(_.toString)
 }
